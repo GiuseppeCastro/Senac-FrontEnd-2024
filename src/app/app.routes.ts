b27f0c9b4cc46de2280932711eea8076ac06b5db
GiuseppeCastro <peppepretto@gmail.com>
2024-04-08 08:28:43 -0300

    Commit apos reset

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'cartas', pathMatch: 'full'},
+  {
+    path: 'cartas',
+    loadChildren:() => import('./cartas/cartas.module').then(m=>m.CartasModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
